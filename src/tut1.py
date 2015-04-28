8fa36003f17cf354a46fb109040ee787fea9b9ab
Takuro Wada <taxpon@gmail.com>
2015-04-28 19:24:04 +0900

    Add header description

--- src/tut1.py
+++ src/tut1.py
@@ -1,3 +1,17 @@
+# -*- coding: utf-8; -*-
+#
+# Copyright 2015, Kabuku Inc. http://www.kabuku.co.jp
+#
+# This repository hosts sample codes used in the articles in rinkak (https://www.rinkak.com)
+#
+# <The article for this code>
+# https://www.rinkak.com/blog/blender-python-modeling-1
+#
+
+__author__ = "Takuro Wada"
+__copyright__ = "Copyright 2015, Kabuku Inc."
+__license__ = "MIT"
+
 import bpy
 
 
